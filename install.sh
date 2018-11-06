ff03caf6b16a0c2c1528681101dd785470a8262a
mrrbrilliant <mrrbrilliantwork@gmail.com>
2018-11-06 22:12:03 +0700

    update

--- install.sh
+++ install.sh
@@ -7,13 +7,16 @@ sudo apt-get install --no-install-recommends obconf -y
 sudo apt-get install --no-install-recommends obmenu -y
 sudo apt-get install --no-install-recommends tint2 -y
 sudo apt-get install --no-install-recommends nitrogen -y
-sudo apt-get install --no-install-recommends plank -y 
+sudo apt-get install --no-install-recommends plank -y
 sudo apt-get install --no-install-recommends pcmanfm -y
 sudo apt-get install --no-install-recommends firefox -y
 sudo apt-get install --no-install-recommends volti -y
-sudo apt-get install --no-install-recommends wicd -y 
+sudo apt-get install --no-install-recommends wicd -y
 sudo apt-get install --no-install-recommends lxappearance -y
 sudo apt-get install --no-install-recommends compton -y
+sudo apt-get install --no-install-recommends build-essential -y
+sudo apt-get install --no-install-recommends cpanminus -y
+sudo apt-get install libgtk2-perl -y
 sudo add-apt-repository ppa:tista/adapta -y
 sudo add-apt-repository ppa:snwh/ppa -y
 sudo apt update -y
@@ -23,6 +26,32 @@ mkdir /home/kosmos/.config
 mkdir /home/kosmos/.config/openbox
 mkdir /home/kosmos/.config/tint2
 cp /home/kosmos/kosmosde/autostart.sh /home/kosmos/.config/openbox/autostart.sh
-cp /home/kosmos/kosmosde/rc.xml /home/kosmos/.config/openbox/
 cp /home/kosmos/kosmosde/kosmos_panel.tint2rc /home/kosmos/.config/tint2/
+cp /home/kosmos/kosmosde/kosmos.svg /home/kosmos/.config/tint2/
+
+# INSTALL JGMENU ------------------------------------------------------------------
+
+git clone https://github.com/johanmalm/jgmenu.git
+rm /home/kosmos/jgmenu/data/jgmenu.svg
+cp /home/kosmos/kosmosde/kosmos.svg /home/kosmos/jgmenu/data/
+mv /home/kosmos/jgmenu/data/kosmos.svg /home/kosmos/jgmenu/data/jgmenu.svg
+cd /home/kosmos/jgmenu/
+./scripts/install-debian-dependencies.sh
+make
+sudo make prefix=/usr install
+
+# INSTALL OBMENU-GENERATOR
+cd /home/kosmos/
+git clone git://github.com/trizen/obmenu-generator
+sudo cp obmenu-generator/obmenu-generator /usr/bin
+mkdir /home/kosmos/.config/obmenu-generator
+sudo cp -r obmenu-generator/schema.pl ~/.config/obmenu-generator/
+sudo cpanm Linux::DesktopFiles
+sudo cpanm Data::Dump
+sudo chmod 777 /usr/bin/obmenu-generator
+sudo obmenu-generator -d -i
+openbox --reconfigure
+
+sudo apt update
+sudo apt upgrade
 reboot
