cf35756790d01e6d5101c196936f74e526313af1
mrrbrilliant <mrrbrilliantwork@gmail.com>
2018-11-06 22:44:45 +0700

    update for Debian

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 compton &
 nitrogen --restore &
-tint2 -c /home/kosmos/.config/tint2/kosmos_panel.tint2rc &
+tint2 -c /home/$USER/.config/tint2/kosmos_panel.tint2rc &
 plank &
 volti &
--- install.sh
+++ install.sh
@@ -22,35 +22,38 @@ sudo add-apt-repository ppa:snwh/ppa -y
 sudo apt update -y
 sudo apt install adapta-gtk-theme -y
 sudo apt install paper-icon-theme -y
-mkdir /home/kosmos/.config
-mkdir /home/kosmos/.config/openbox
-mkdir /home/kosmos/.config/tint2
-cp /home/kosmos/kosmosde/autostart.sh /home/kosmos/.config/openbox/autostart.sh
-cp /home/kosmos/kosmosde/kosmos_panel.tint2rc /home/kosmos/.config/tint2/
-cp /home/kosmos/kosmosde/kosmos.svg /home/kosmos/.config/tint2/
+mkdir /home/$USER/.config
+mkdir /home/$USER/.config/openbox
+mkdir /home/$USER/.config/tint2
+cp /home/$USER/kosmosde/autostart.sh /home/$USER/.config/openbox/autostart.sh
+cp /home/$USER/kosmosde/kosmos_panel.tint2rc /home/$USER/.config/tint2/
+cp /home/$USER/kosmosde/kosmos.svg /home/$USER/.config/tint2/
 
 # INSTALL JGMENU ------------------------------------------------------------------
 
 git clone https://github.com/johanmalm/jgmenu.git
-rm /home/kosmos/jgmenu/data/jgmenu.svg
-cp /home/kosmos/kosmosde/kosmos.svg /home/kosmos/jgmenu/data/
-mv /home/kosmos/jgmenu/data/kosmos.svg /home/kosmos/jgmenu/data/jgmenu.svg
-cd /home/kosmos/jgmenu/
+rm /home/$USER/jgmenu/data/jgmenu.svg
+cp /home/$USER/kosmosde/kosmos.svg /home/$USER/jgmenu/data/
+mv /home/$USER/jgmenu/data/kosmos.svg /home/$USER/jgmenu/data/jgmenu.svg
+cd /home/$USER/jgmenu/
 ./scripts/install-debian-dependencies.sh
 make
 sudo make prefix=/usr install
 
 # INSTALL OBMENU-GENERATOR
-cd /home/kosmos/
+cd /home/$USER/
 git clone git://github.com/trizen/obmenu-generator
-sudo cp obmenu-generator/obmenu-generator /usr/bin
-mkdir /home/kosmos/.config/obmenu-generator
-sudo cp -r obmenu-generator/schema.pl ~/.config/obmenu-generator/
+sudo cp /home/$USER/obmenu-generator/obmenu-generator /usr/bin
+mkdir /home/$USER/.config/obmenu-generator
+sudo cp -r /home/$USER/obmenu-generator/schema.pl /home/$USER/.config/obmenu-generator/
 sudo cpanm Linux::DesktopFiles
 sudo cpanm Data::Dump
 sudo chmod 777 /usr/bin/obmenu-generator
 sudo obmenu-generator -d -i
 openbox --reconfigure
+
+cp /home/$USER/kosmosde/kosmos.jpg /home/$USER/.config/openbox/
+nitrogen --set-zoom-fill /home/$USER/.config/openbox/kosmos.jpg --save
 
 sudo apt update
 sudo apt upgrade
