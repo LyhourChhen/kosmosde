23d94c4f122c2b84be9a644f396ed99446814579
mrrbrilliant <mrrbrilliantwork@gmail.com>
2018-11-06 17:39:00 +0700

    update

--- install.sh
+++ install.sh
@@ -19,10 +19,9 @@ sudo add-apt-repository ppa:snwh/ppa -y
 sudo apt update -y
 sudo apt install adapta-gtk-theme -y
 sudo apt install paper-icon-theme -y
-git clone https://github.com/johanmalm/jgmenu.git
-mkdir .config
-mkdir .config/openbox
-mkdir .config/tint2
+mkdir /home/kosmos/.config
+mkdir /home/kosmos/.config/openbox
+mkdir /home/kosmos/.config/tint2
 cp /home/kosmos/kosmosde/autostart.sh /home/kosmos/.config/openbox/autostart.sh
 cp /home/kosmos/kosmosde/rc.xml /home/kosmos/.config/openbox/
 cp /home/kosmos/kosmosde/kosmos_panel.tint2rc /home/kosmos/.config/tint2/
